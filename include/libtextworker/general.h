a01d631a2cc8dd587644437cd854ce378de5c18f
lebao3105 <bao12345yocoo@gmail.com>
2024-03-04 23:04:26 +0700

    Untested: md5 hash

--- include/libtextworker/general.h
+++ include/libtextworker/general.h
@@ -1,15 +1,13 @@
 #include <exception>
 #include <vector>
 #include <stdarg.h>
+#include <openssl/md5.h>
 
 namespace libtextworker
 {
     namespace general
     {
-        /*
-            @class libtextworker.libTewException
-            @brief The base class for libtextworker exceptions.
-        */
+        /* The base class for libtextworker exceptions. */
         class libTewException : public std::exception
         {
         private:
@@ -25,34 +23,35 @@ namespace libtextworker
                 message = oss.str();
             }
 
-            const std::string what()
-            {
-                return message;
-            }
+            const std::string what() { return message; }
         };
 
-        /*
-            @brief Crafts any >=2 path, together.
-            @param args (std::vector of char*): Strings to use.
-            @return The generated path.
+        /* Crafts any >=2 path, together.
+           @param args (std::vector of char*): Strings to use.
+           @return The generated path.
         */
         char *CraftItems(std::vector<char *> args);
 
-        /*
-            @brief Creates a directory with subfolders inside (optional).
-            @param directory (char*): Target parent directory
-            @param childs (std::vector of char*): Folder childrends
-            @return Status of the progress. 0 is success.
-            @throws libTewException: On directory create error
+        /* Creates a directory with subfolders inside (optional).
+           @param directory (char*): Target parent directory
+           @param childs (std::vector of char*): Folder childrends
+           @return Status of the progress. 0 is success.
+           @throws libTewException: On directory create error
         */
         int CreateDirectory(char *directory, std::vector<char *> childs = {});
 
-        /*
-            @brief Creates a directory with subfolders inside, layer-to-layer.
-            @paragraph For example: path1/path2/path3. This function will create path1, path1/path2, then path1/path2/path3. Skip existing folders.
-            @param path (char*): Target folder path.
-            @return Status of the progress. 0 is success.
+        /* Creates a directory with subfolders inside, layer-to-layer.
+           @paragraph For example: path1/path2/path3. This function will create path1, path1/path2, then path1/path2/path3. Skip existing folders.
+           @param path (char*): Target folder path.
+           @return Status of the progress. 0 is success.
         */
         int WalkCreation(char *path);
+
+        /* Undocumented */
+        unsigned char* MD5Hash(unsigned char* string) {
+            unsigned char result[MD5_DIGEST_LENGTH];
+            MD5(string, sizeof(string) / sizeof(unsigned char*), result);
+            return result;
+        };
     }
 }
